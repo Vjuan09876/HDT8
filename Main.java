749883a183caf04ff30d687a28c208c37a51c755
Vjuan09876 <jruan926@gmail.com>
2026-04-01 17:30:25 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Paciente> colaEmergencias = new VectorHeap<>();
+
+        System.out.println("leyendo pacientes");
+        try (BufferedReader br = new BufferedReader(new FileReader("pacientes.txt"))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(",");
+                if (partes.length == 3) {
+                    Paciente p = new Paciente(partes[0], partes[1], partes[2]);
+                    colaEmergencias.add(p);
+                }
+            }
+        } 
+        }
+        System.out.println("\n atendiendo pacientes");
+        while (!colaEmergencias.isEmpty()) {
+            Paciente atendido = colaEmergencias.remove();
+            System.out.println(atendido.toString());
+        }
+    }
